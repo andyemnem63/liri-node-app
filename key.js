22bbd599d4b82307cd573ac52465b55c075f2a4c
AndyEm <andyem64@gmail.com>
2017-04-02 21:30:18 -0400

    added comments

--- key.js
+++ key.js
@@ -1,52 +1,6 @@
-console.log('this is loaded');
-//Loads the twitter module
-var twitter = require('twitter');
-
-//User authentication credentials
-var client = new twitter({
+ exports.twitterKey = {
   consumer_key: 'THYjJYwnWcZsiOSD0nUntii81',
   consumer_secret: 'IMjkuBQMWDYDatWboq6KbfJwroxi4cFy7uE8N9CpDpwrx8n2Js',
   access_token_key: '2984519260-JstAvQXD5yA7ByT1izPBW3vqksDYgDCq97shIDk',
   access_token_secret: 'GNU9xDgYL5woQzuUaCUKgDQgpNwvQ0AYzrGXiEtEGp8kF'
-});
-
-//Search parameters. Searches by userName
-//A maximum of 21 tweets can be returned
-var params = {screen_name: 'Andy_Em64',
-              count:21
-            };
-
-//Gets most recent tweets posted by the user indicated by the first parameter
-//Second parameter gets data from users screen name
-//Third parameter is a callback function, if credentials are valid then provide resonse else provide error
-exports.twitter = client.get('statuses/user_timeline', params, function(error, tweets, response) {
-       
-        //If there is no error
-        if (!error) {
-            //Return 20 of the users most recent tweets 
-            for(var i = 1; i <= 20; i++){
-              console.log('============================');
-              console.log('TWEET number ' + i);
-              console.log(tweets[i]['text']);
-              console.log('============================');
-            }
-        }
-        //Else, display an error
-        else {
-          console.log(error);
-        }
-});
-
-//Spotify
-//=================================================================================
-var spotify = require('spotify');
-
-spotify.search({type:'track', query: 'I want it that way'}, function(err, data){
-        if(!err){
-            console.log(data['tracks']['items'][0]['album']['artists']);
-        }
-        else{
-          console.log(false);
-        }
-});
-
+};
--- liri.js
+++ liri.js
@@ -1,11 +1,94 @@
+/*Global Variables
+==============================================================*/
 
-//If the 2 index input is the string my-tweets then....
-if(process.argv[2] === "my-tweets"){
-	//Gets key module and display 20 tweetss
-	require('./key').twitter;
-}
-//If the 2 index input is the string my-tweets then....
-else if(process.argv[2] === "spotify-this-song"){
-	console.log(true);
+var spotify = require('spotify');
+var twitter = require('twitter');
+var key = require('./key');
+
+var userInput = process.argv[2];
+var userChoice = process.argv[3];
+
+
+/*Functions
+==============================================================*/
+
+function twitterData(){
+
+    //User authentication credentials
+    var client = new twitter(key.twitterKey);
+
+    //Search parameters. Searches by userName
+    //A maximum of 21 tweets can be returned
+    var params = {  
+                    screen_name: 'Andy_Em64',
+                    count:21
+                 };
+
+    //Gets most recent tweets posted by the user indicated by the first parameter
+    //Second parameter gets data from users screen name
+    //Third parameter is a callback function, if credentials are valid then provide resonse else provide error
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+           
+            //If there is no error...
+            if (!error) {
+                //Return 20 of the users most recent tweets 
+                for(var i = 1; i <= 20; i++){
+                  console.log('TWEET #' + i + ':');
+                  console.log(tweets[i]['text']);
+                  console.log('============================');
+                }
+            }
+            //Else, display an error
+            else {
+              console.log(error);
+            }
+    });
 }
 
+function spotifyData(){
+
+    spotify.search({type:'track', query: userChoice}, function(err, data){
+
+            if(!err){
+                var artists = data['tracks']['items'][1]['artists'][0]['name'];
+                var album = data['tracks']['items'][1]['album']['name'];
+                var songPreview = data['tracks']['items'][1]['external_urls']['spotify'];
+                var track = data['tracks']['items'][1]['name'];
+                //Testing
+                // console.log(JSON.stringify(data['tracks']['items'][1]['name'] ,null, 1));
+                
+                console.log(artists);
+                console.log(track);
+                console.log(album);
+                console.log(songPreview);
+            }
+            else{
+              console.log(err);
+            }
+    });
+}
+
+function movieData(){
+
+  console.log(true);
+}
+/*Main
+==============================================================*/
+
+switch(userInput){
+     
+      case "my-tweets":
+          twitterData();
+          break;
+     
+      case "spotify-this-song":
+          spotifyData();
+          break;
+      
+      case "movie-this":
+        movieData();
+        break;
+
+      default:
+          console.log('Error!!');
+}

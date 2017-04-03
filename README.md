41a891875cc42f6eb899254fe8a8fd9e752e1299
AndyEm <andyem64@gmail.com>
2017-04-03 11:13:13 -0400

    read file section working

--- README.md
+++ README.md
@@ -1,19 +1,6 @@
 # liri-node-app
 
   
-If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-
-  * Title of the movie.
-  * Year the movie came out.
-  * IMDB Rating of the movie.
-  * Country where the movie was produced.
-  * Language of the movie.
-  * Plot of the movie.
-  * Actors in the movie.
-  * Rotten Tomatoes Rating.
-  * Rotten Tomatoes URL.
-
-  If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
 
 
   node dixby.js do-what-it-says
@@ -22,10 +9,11 @@ Using the fs Node package, DIXBY will take the text inside of random.txt and the
 It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
 Feel free to change the text in that document to test out the feature for other commands.
 
-BONUS
 
 In addition to logging the data to your terminal/bash window, output the data to a .txt file called log.txt.
 
 Make sure you append each command you run to the log.txt file.
 
 Do not overwrite your file each time you run a command.
+
+If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'	
--- liri.js
+++ liri.js
@@ -70,7 +70,7 @@ function spotifyData(){
 }
 
 function movieData(){
-    request('http://www.omdbapi.com/?t=' + 'spiderman',function(error, response, body){
+    request('http://www.omdbapi.com/?t=' + userChoice,function(error, response, body){
         if (!error && response.statusCode == 200) {
           var parsedData = JSON.parse(body);
           
@@ -82,19 +82,17 @@ function movieData(){
           var moviePlot = parsedData['Plot'];
           var movieActor = parsedData['Actors'];
           var movieTomatoValue = parsedData['Ratings'][1]['Value'];
-          var movieTomato = parsedData['Ratings'][1]['Source'];
           
           //Movie info
-          console.log(movieTitle);
-          console.log(movieYear);
-          console.log(movieRating);
-          console.log(movieCountry);
-          console.log(movieLang);
-          console.log(moviePlot);
-          console.log(movieActor);
-          //console.log(movieTomato + ' ' + movieTomatoValue);
-          // //Testing
-          //console.log(parsedData);
+          console.log('Movie Title: ' + movieTitle);
+          console.log('Year: ' + movieYear);
+          console.log('Imdb Rating: ' + movieRating);
+          console.log('Country: ' + movieCountry);
+          console.log('Language: ' + movieLang);
+          console.log('Plot: ' + moviePlot);
+          console.log('Actor: ' + movieActor);
+          console.log('Rotton Tomatoes ' + movieTomatoValue);
+          //Testing
           console.log(parsedData);
         }
         else if(error){
@@ -102,11 +100,44 @@ function movieData(){
         }
     });
 }
+function readData(){
+
+var fs = require('fs');
+
+  fs.readFile('random.txt','utf8', function(err,data){
+      if(err) throw err;
+
+      var dataSplit = data.split(',');
+          userInput = dataSplit[0];
+          userChoice = dataSplit[1];
+
+      switch(userInput){
+
+          case "my-tweets":
+            twitterData();
+            break;
+
+          case "spotify-this-song":
+            spotifyData();
+            break;
+        
+          case "movie-this":
+            movieData();
+            break;
+      }
+
+  });
+
+}
 /*Main
 ==============================================================*/
 
 switch(userInput){
      
+      case "do":
+          readData();
+          break;
+      
       case "my-tweets":
           twitterData();
           break;
@@ -116,8 +147,9 @@ switch(userInput){
           break;
       
       case "movie-this":
-        movieData();
-        break;
+          movieData();
+          break;
+
 
       default:
           console.log('Error!!');

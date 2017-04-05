5f08a1ff95e545753247303ee3e7a6da54c748a2
AndyEm <andyem64@gmail.com>
2017-04-05 15:40:15 -0400

    added function to disply movie and song info + added method to go to default song or movie when user enters nothing in

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
 # liri-node-app
 
-  
+ 1. Spotify, If no song is provided then run "The Sign" by Ace of Base
+
+ 2. If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.
 
 
-  node dixby.js do-what-it-says
-
-Using the fs Node package, DIXBY will take the text inside of random.txt and then use it to call one of DIXBY's commands.
-It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-Feel free to change the text in that document to test out the feature for other commands.
-
+ BONUS
 
 In addition to logging the data to your terminal/bash window, output the data to a .txt file called log.txt.
 
@@ -16,4 +13,3 @@ Make sure you append each command you run to the log.txt file.
 
 Do not overwrite your file each time you run a command.
 
-If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'	
--- liri.js
+++ liri.js
@@ -10,7 +10,7 @@ var request = require('request');
 var userInput = process.argv[2];
 var userChoice = process.argv[3];
 
-
+//Movie Info
 var movieTitle;
 var movieYear;
 var movieRating;
@@ -58,59 +58,96 @@ function twitterData(){
 }
 
 function spotifyData(){
+      
+        if(process.argv.length >=4){
 
-    spotify.search({type:'track', query: userChoice}, function(err, data){
+            spotify.search({type:'track', query: userChoice}, function(err, data){
+                  if(!err) {
+                    displaySpotify(data);
+                  }
+               
+                  else {
+                    throw err;
+                  }
+            });
+        }
+        else if(process.argv.length < 4){
+            spotify.search({type:'track', query: "The Sign Ace of Base" }, function(err, data){
+                if(!err) {
+                  displaySpotify(data);
+                }
+             
+                else {
+                  throw err;
+                }
+          });
+        }
+}
 
-            if(err) throw err;
-
-                var artists = data['tracks']['items'][1]['artists'][0]['name'];
-                var album = data['tracks']['items'][1]['album']['name'];
-                var songPreview = data['tracks']['items'][1]['external_urls']['spotify'];
-                var track = data['tracks']['items'][1]['name'];
-                //Testing
-                // console.log(JSON.stringify(data['tracks']['items'][1]['name'] ,null, 1));
-                
-                console.log('Artist: ' + artists);
-                console.log('Track: ' + track);
-                console.log('Album ' + album);
-                console.log('Song Preview: ' + songPreview);
-    });
+function displaySpotify(data){
+      var artists = data['tracks']['items'][1]['artists'][0]['name'];
+      var album = data['tracks']['items'][1]['album']['name'];
+      var songPreview = data['tracks']['items'][1]['external_urls']['spotify'];
+      var track = data['tracks']['items'][1]['name'];
+      //Testing
+      // console.log(JSON.stringify(data['tracks']['items'][1]['name'] ,null, 1));
+      console.log('Artist: ' + artists);
+      console.log('Track: ' + track);
+      console.log('Album ' + album);
+      console.log('Song Preview: ' + songPreview);
 }
 
 function movieData(){
-    request('http://www.omdbapi.com/?t=' + userChoice +'&tomatoes=true',function(error, response, body){
-        if (!error && response.statusCode == 200) {
-          var parsedData = JSON.parse(body);
-          //Testing
-          // console.log(parsedData);
-          
-          movieTitle = parsedData['Title'];
-          movieYear = parsedData['Year'];
-          movieRating = parsedData['imdbRating'];
-          movieCountry = parsedData['Country'];
-          movieLang = parsedData['Language'];
-          moviePlot = parsedData['Plot'];
-          movieActor = parsedData['Actors'];
-          tomatoRating = parsedData['tomatoRating'];
-          tomatoURL = parsedData['tomatoURL'];
-          //Movie info
-          console.log('Movie Title: ' + movieTitle);
-          console.log('Year: ' + movieYear);
-          console.log('Imdb Rating: ' + movieRating);
-          console.log('Country: ' + movieCountry);
-          console.log('Language: ' + movieLang);
-          console.log('Plot: ' + moviePlot);
-          console.log('Actor: ' + movieActor);
-          console.log('Rotton Tomatoes Rating: ' + tomatoRating);
-          console.log('Rotton URL: ' + tomatoURL);
-        }
-        else if(error){
-          console.log('no movies');
-        }
-    });
+    if(process.argv.length >=4)    
+        request('http://www.omdbapi.com/?t=' + userChoice +'&tomatoes=true',function(error, response, body){
+            if (!error && response.statusCode == 200) {
+                  var parsedData = JSON.parse(body);
+                  displayMovie(parsedData);
+              
+            }
+            else{
+              throw error;
+            }
+        });
+    else if(process.argv.length < 4){
+        request('http://www.omdbapi.com/?t=' + 'Mr. Nobody' +'&tomatoes=true',function(error, response, body){
+            if (!error && response.statusCode == 200) {
+                  var parsedData = JSON.parse(body);
+                  displayMovie(parsedData);
+              
+            }
+            else {
+              throw error;
+            }
+        });
+    }
+}
+
+function displayMovie(parsedData){
+
+      movieTitle = parsedData['Title'];
+      movieYear = parsedData['Year'];
+      movieRating = parsedData['imdbRating'];
+      movieCountry = parsedData['Country'];
+      movieLang = parsedData['Language'];
+      moviePlot = parsedData['Plot'];
+      movieActor = parsedData['Actors'];
+      tomatoRating = parsedData['tomatoRating'];
+      tomatoURL = parsedData['tomatoURL'];
+      //Movie info
+      console.log('Movie Title: ' + movieTitle);
+      console.log('Year: ' + movieYear);
+      console.log('Imdb Rating: ' + movieRating);
+      console.log('Country: ' + movieCountry);
+      console.log('Language: ' + movieLang);
+      console.log('Plot: ' + moviePlot);
+      console.log('Actor: ' + movieActor);
+      console.log('Rotton Tomatoes Rating: ' + tomatoRating);
+      console.log('Rotton URL: ' + tomatoURL);
+
 }
 function readData(){
-s
+
 var fs = require('fs');
 
   fs.readFile('random.txt','utf8', function(err,data){
